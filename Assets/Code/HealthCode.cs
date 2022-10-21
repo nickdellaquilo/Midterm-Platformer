078bbc52eb0a138ff8edf9fdfaa70df24ac83baf
jx1279 <jx1279@nyu.edu>
2022-10-20 21:51:36 -0400

    Updating code for player health

---
+++ Assets/Code/HealthCode.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthCode : MonoBehaviour
+{
+    [SerializeField] private GameObject[] hearts;
+    private int life;
+    private bool dead;
+    private Animator anim;
+
+    private void Start()
+    {
+        life = hearts.Length;
+    }
+
+    void Update()
+    {
+        if (dead)
+        {
+            //implement death sequence
+            
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (life >= 1)
+        {
+            life -= damage;
+            hearts[life].gameObject.SetActive(false);
+            if (life < 1)
+            {
+                isDead = true;
+            }
+            
+        }
+    }
+}
