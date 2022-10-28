250df1982a501aece3e5b12be9482afaec8c0a1d
jx1279 <jx1279@nyu.edu>
2022-10-28 08:43:01 -0400

    Door Updated

--- Assets/Code/DoorCode.cs
+++ Assets/Code/DoorCode.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DoorCode : MonoBehaviour
 {
+    public string levelName = "Scene2";
+    
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")) {
+                
+                SceneManager.LoadScene(levelName);
+            }
+            
+        }
+    
     // Start is called before the first frame update
     void Start()
     {
