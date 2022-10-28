2926c4b157f509200bda033f34c340798e3cfc0c
Cameron Vong <cameronvong@gmail.com>
2022-10-28 06:40:19 -0400

    added bullet

--- Assets/Code/Bullet.cs
+++ Assets/Code/Bullet.cs
@@ -3,3 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    private bool hit;
+
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+    }
+
+    private void Update()
+    {
+        if (hit) return;
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
+
+    }
+
+    private void OnTriggerEnter(Collider2D collision) 
+    {
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("shoot");
+
+        
+    }
+
+    public void SetDirection(float _direction) 
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction) 
+            localScaleX = -localScaleX;
+        
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate() 
+    {
+        gameObject.SetActive(false);
+    }
+
+
+}
